22e0a5b6656c0c24d29b31ccc6b41686c5109193
Luc Mosser <luc.mosser86@orange.fr>
2020-06-14 23:02:33 +0200

    first commit

--- app.js
+++ app.js
@@ -6,38 +6,38 @@ const startBtn = document.querySelector('#start-button')
 const width = 10
 const heigth = 20
 
-const lTetromino = [
-  [1, width+1, width*2+1, 2],
-  [width, width+1, width+2, width*2+2],
-  [1, width+1, width*2+1, width*2],
-  [width, width*2, width*2+1, width*2+2]
-]
+  const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
 
-const zTetromino = [
-  [width+1, width+2, width*2, width*2+1],
-  [0, width, width+1, width*2+1],
-  [width+1, width+2, width*2, width*2+1],
-  [0, width, width+1, width*2+1],
-]
+  const zTetromino = [
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
+  ]
 
-const tTetromino = [
-  [1, width+1, width*2+1, 2],
-  [width, width+1, width+2, width*2+2],
-  [1, width+1, width*2+1, width*2],
-  [width, width*2, width*2+1, width*2+2]
-]
+  const tTetromino = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ]
 
-const oTetromino = [
-  [1, width+1, width*2+1, 2],
-  [width, width+1, width+2, width*2+2],
-  [1, width+1, width*2+1, width*2],
-  [width, width*2, width*2+1, width*2+2]
-]
+  const oTetromino = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ]
 
-const iTetromino = [
-  [1, width+1, width*2+1, 2],
-  [width, width+1, width+2, width*2+2],
-  [1, width+1, width*2+1, width*2],
-  [width, width*2, width*2+1, width*2+2]
-]
+  const iTetromino = [
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+  ]
 })
